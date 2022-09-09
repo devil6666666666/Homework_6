2e16812561b842133ad8314b50ecaae6e0cf4fd0
Viktor A <abcd999@bk.ru>
2022-09-10 00:26:48 +0300

    Дополнительная задача на рекурсию

---
+++ Task_Additional/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N - через рекурсию.
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int prod = 1;
+void PrintNumbers(int number)
+{
+    if (number == 0)
+    {
+        return;
+    }
+    PrintNumbers(number - 1);
+    prod = prod * number;
+}
+PrintNumbers(n);
+Console.WriteLine($"Произведение чисел от 1 до N = {prod}");

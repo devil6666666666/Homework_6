05011a91d69bb7eac9df4cc35af83687a1e43ca5
Viktor A <abcd999@bk.ru>
2022-09-10 00:07:49 +0300

    Задача 45

---
+++ Task_45/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного копирования.
+
+int[] array = new int[10] { 7, -28, 587, 43, -486, 93, 107, -72, 7, 2 };
+int[] newArray = new int[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    newArray[i] = array[i];
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+Console.WriteLine('[' + string.Join(", ", newArray) + ']');

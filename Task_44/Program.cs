84f6d7b1c0db5c6854452d3dfe1fb3ff149b2050
Viktor A <abcd999@bk.ru>
2022-09-09 23:37:04 +0300

    Задача 44

---
+++ Task_44/Program.cs
@@ -0,0 +1,12 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи.
+
+Console.Write("Введите количество отображаемых чисел Фибоначчи: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+array[0]=0;
+array[1]=1;
+for (int count = 2; count < N; count++)
+{
+    array[count] = array[count-1]+array[count-2];
+}
+Console.WriteLine(string.Join(" ", array));
